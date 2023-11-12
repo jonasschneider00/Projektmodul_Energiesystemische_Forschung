201aae36d4d5253ddcaaaf26513f4fde7370cf16
Jonas Schneider <jonas.schneider1@rwth-aachen.de>
2023-11-13 00:31:20 +0100

    lkws werden jetzt bis 80 prozent geladen

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 
-timesteps = 15
-timedelta = 10
+timesteps = 100
+timedelta = 1
 anzahl_ladesäulen = 3
 
-data = {'Ankunftszeit': [0, 0, 0, 0, 20, 30, 30],
-        'Kapazität': [600, 800, 800, 800, 600, 600, 700],
+data = {'Ankunftszeit': [0, 0, 0, 0, 8, 9, 14],
+        'Kapazität': [100, 200, 300, 400, 500, 600, 700],
         'Akkustand': [10, 12, 30, 22, 24, 14, 21]}
 lkws = pd.DataFrame(data)
 
@@ -50,10 +50,21 @@ def getladeleistung(ladestand, ladekurve):
 
 
 def laden(df_ladesäulen_t, lkws_in_timestep, timestep):
-    df_t1 = df_ladesäulen_t
+    df_t1 = df_ladesäulen_t.copy()
     summe_ladender_lkws = 0
+    if timestep > 0:
+        for l , ladesäule in enumerate(df_t1.columns):
+            lkws_t_minus_1 = df_t1.at[timestep - timedelta, ladesäule]
+            lkws_t_0 = []
+            for lkw in lkws_t_minus_1:
+                lkw_copy = lkw[:]
+                if lkw_copy[0] < 80:
+                    lkw_t_0 = lade_lkw(lkw_copy)
+                    lkws_t_0 += lkw_t_0
+                    summe_ladender_lkws += 1
+            df_t1.at[timestep, ladesäule] = lkws_t_0
     if len(lkws_in_timestep) == 0:
-        return df_ladesäulen_t
+        return df_t1
     else:
         for lkw in lkws_in_timestep:
             for ladesäule, wert in df_t1.loc[t].items():
@@ -69,6 +80,10 @@ def laden(df_ladesäulen_t, lkws_in_timestep, timestep):
     return df_t1
 
 
+def lade_lkw(lkw):
+    lkw_t1 = lkw
+    lkw_t1[0] += 5
+    return [lkw_t1]
 # def lademanegement(anzahl_ladender_lkw):
 #   return dummy=0
 
