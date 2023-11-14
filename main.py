e97b46039094b29015c9c4f20184fe10ffbe39cb
Jonas Schneider <jonas.schneider1@rwth-aachen.de>
2023-11-14 11:34:53 +0100

    ladekurve funktioniert

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
-timesteps = 100
-timedelta = 1
+timesteps = 20
+timedelta = 5 #in min
 anzahl_ladesäulen = 3
+max_akkustand = 80 #heißt 80 %
 
-data = {'Ankunftszeit': [0, 0, 0, 0, 8, 9, 14],
+data = {'Ankunftszeit': [0, 0, 0, 0, 5, 10, 50],
         'Kapazität': [100, 200, 300, 400, 500, 600, 700],
-        'Akkustand': [10, 12, 30, 22, 24, 14, 21]}
+        'Akkustand': [9, 16, 30, 22, 51, 14, 21]}
 lkws = pd.DataFrame(data)
 
 
@@ -31,9 +32,15 @@ def sortiere_lkws_nach_timstep(lkws):
 
 
 def erstelle_Ladekurve():
-    num_rows = 101
-    cell_value = 600
-    df = pd.DataFrame({'Ladeleistung': [cell_value] * num_rows})
+    #num_rows = 101
+    #cell_value = 600
+    #df = pd.DataFrame({'Ladeleistung': [cell_value] * num_rows})
+    daten = []
+    for i in range(0, 100, 10):
+        for j in range(i, i + 10):
+            if j <= 100:
+                daten.append((j, 600 - (i // 10) * 10))
+    df = pd.DataFrame(daten, columns=['Index', 'Ladeleistung'])
     return df
 
 
@@ -53,15 +60,17 @@ def laden(df_ladesäulen_t, lkws_in_timestep, timestep):
     df_t1 = df_ladesäulen_t.copy()
     summe_ladender_lkws = 0
     if timestep > 0:
-        for l , ladesäule in enumerate(df_t1.columns):
+        for l, ladesäule in enumerate(df_t1.columns):
             lkws_t_minus_1 = df_t1.at[timestep - timedelta, ladesäule]
+            ladeleistungen = lademanegement(lkws_t_minus_1=lkws_t_minus_1)
             lkws_t_0 = []
-            for lkw in lkws_t_minus_1:
+            for i,lkw in enumerate(lkws_t_minus_1):
                 lkw_copy = lkw[:]
-                if lkw_copy[0] < 80:
-                    lkw_t_0 = lade_lkw(lkw_copy)
-                    lkws_t_0 += lkw_t_0
-                    summe_ladender_lkws += 1
+                if lkw_copy[0] < max_akkustand:
+                    lkw_t_0 = lade_lkw(lkw=lkw_copy, ladeleistung=ladeleistungen[i])
+                    if lkw_t_0[0][0] < max_akkustand:
+                        lkws_t_0 += lkw_t_0
+                        summe_ladender_lkws += 1
             df_t1.at[timestep, ladesäule] = lkws_t_0
     if len(lkws_in_timestep) == 0:
         return df_t1
@@ -80,12 +89,22 @@ def laden(df_ladesäulen_t, lkws_in_timestep, timestep):
     return df_t1
 
 
-def lade_lkw(lkw):
+def lade_lkw(lkw, ladeleistung):
     lkw_t1 = lkw
-    lkw_t1[0] += 5
+    lkw_t1[0] += round(((ladeleistung*timedelta/60)/lkw_t1[1])*100, 2)
     return [lkw_t1]
-# def lademanegement(anzahl_ladender_lkw):
-#   return dummy=0
+
+
+def lademanegement(lkws_t_minus_1):
+    ladeleistungen = []
+    if len(lkws_t_minus_1) == 1:
+        ladeleistungen.append(getladeleistung(ladestand=round(lkws_t_minus_1[0][0]), ladekurve=erstelle_Ladekurve()))
+    elif len(lkws_t_minus_1) == 2:
+        for lkw in lkws_t_minus_1:
+            ladeleistung = 0.5 * getladeleistung(ladestand=round(lkw[0]), ladekurve=erstelle_Ladekurve())
+            ladeleistungen.append(ladeleistung)
+    return ladeleistungen
+
 
 if __name__ == '__main__':
     df = create_dataframe_with_dimensions(num_rows=timesteps, num_columns=anzahl_ladesäulen)
